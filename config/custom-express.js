f94f546d17911035b2040551a80cc9d366fc2786
Bruno Negreiros <brunoananias_n@hotmail.com>
2020-04-03 17:35:49 -0400

    Aplicando validações na entrada.

--- config/custom-express.js
+++ config/custom-express.js
@@ -1,11 +1,15 @@
 var express = require('express');
 var consign = require('consign');
 var bodyParser = require('body-parser');
+var expressValidator = require('express-validator');
 
 module.exports = function() {
   var app = express();
 
   app.use(bodyParser.json());
+
+  //Obrigatoriamente logo apos o bodyParser
+  app.use(expressValidator());
 
   consign()
    .include('controllers')
--- controllers/pagamentos.js
+++ controllers/pagamentos.js
@@ -6,7 +6,21 @@ module.exports = function(app) {
   });
 
   app.post('/pagamentos/pagamento', function(req, resp) {
-    console.log('Requisição POST interceptada na rota /pagamentos/pagamento.')
+    console.log('Requisição POST interceptada na rota /pagamentos/pagamento.');
+
+    req.assert('forma_de_pagamento', 'Obrigatório informar forma de pagamento.').notEmpty();
+    req.assert('valor', 'Obrigatório informar o valor e o mesmo deve ser um decimal').notEmpty().isFloat();
+    req.assert('moeda', 'Obrigatório informar a moeda e a mesma deve ter 3 caracteres').notEmpty().len(3,3);
+
+    var errors = req.validationErrors();
+
+    if (errors) {
+      console.log('Erros na validação encontrados.');
+      resp.status(400).send(errors);
+      return;
+    }
+    console.log('Processando pagamento.');
+
     var pagamento = req.body;
 
     pagamento.status = "CRIADO";
@@ -17,7 +31,8 @@ module.exports = function(app) {
 
     pagamentoDao.salva(pagamento, function(erro, resultado) {
       if (erro) {
-        resp.send(erro);
+        console.log('Erro ao salvar no banco: ' + erro);
+        resp.status(400).send(erro);
       } else {
         console.log('pagamento criado');
         resp.json(pagamento);
